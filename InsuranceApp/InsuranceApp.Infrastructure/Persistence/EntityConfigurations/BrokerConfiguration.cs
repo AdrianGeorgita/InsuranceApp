23f6ff33263acb484ae7857cd8a183840d85d471
AdrianGeorgita <georgita.adrian@gmail.com>
2026-03-05 11:28:42 +0200

    Move DbContext Configurations to separate files

---
+++ InsuranceApp/InsuranceApp.Infrastructure/Persistence/EntityConfigurations/BrokerConfiguration.cs
@@ -0,0 +1,42 @@
+﻿using InsuranceApp.Domain.Entities;
+using InsuranceApp.Domain.Enums;
+using InsuranceApp.Infrastructure.Persistence.Constants;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace InsuranceApp.Infrastructure.Persistence.EntityConfigurations;
+
+public class BrokerConfiguration : IEntityTypeConfiguration<Broker>
+{
+    public void Configure(EntityTypeBuilder<Broker> builder)
+    {
+        builder.HasKey(e => e.Id).HasName("PK__Brokers__3214EC07BEE2B83E");
+
+        builder.HasIndex(e => e.Code, "UQ_Brokers_Code").IsUnique();
+
+        builder.HasIndex(e => e.Email, "UQ_Brokers_Email").IsUnique();
+
+        builder.HasIndex(e => e.Phone, "UQ_Brokers_Phone").IsUnique();
+
+        builder.Property(e => e.Id).ValueGeneratedNever();
+        builder.Property(e => e.Code).HasMaxLength(128);
+        builder.Property(e => e.CommissionPercentage)
+            .HasDefaultValue(0.0m)
+            .HasColumnType("decimal(10, 6)");
+        builder.Property(e => e.CreatedAt)
+            .HasPrecision(0)
+            .HasDefaultValueSql(DbConstants.SysUtcDateTime);
+        builder.Property(e => e.Email).HasMaxLength(256);
+        builder.Property(e => e.Name).HasMaxLength(256);
+        builder.Property(e => e.Phone).HasMaxLength(15);
+        builder.Property(e => e.UpdatedAt)
+            .HasPrecision(0)
+            .IsRowVersion()
+            .HasDefaultValueSql(DbConstants.SysUtcDateTime);
+        builder.Property(e => e.Status)
+            .HasConversion(
+                v => v == BrokerStatus.Active,
+                v => v ? BrokerStatus.Active : BrokerStatus.Inactive
+            );
+    }
+}
---
+++ InsuranceApp/InsuranceApp.Infrastructure/Persistence/EntityConfigurations/RiskIndicatorConfiguration.cs
@@ -0,0 +1,25 @@
+﻿using InsuranceApp.Domain.Entities;
+using InsuranceApp.Infrastructure.Persistence.Constants;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace InsuranceApp.Infrastructure.Persistence.EntityConfigurations;
+
+public class RiskIndicatorConfiguration : IEntityTypeConfiguration<RiskIndicator>
+{
+    public void Configure(EntityTypeBuilder<RiskIndicator> builder)
+    {
+        builder.HasKey(e => e.Id).HasName("PK__RiskIndi__3214EC0705F8A6BF");
+
+        builder.HasIndex(e => e.Name, "UQ_RiskIndicators_Name").IsUnique();
+
+        builder.Property(e => e.CreatedAt)
+            .HasPrecision(0)
+            .HasDefaultValueSql(DbConstants.SysUtcDateTime);
+        builder.Property(e => e.Name).HasMaxLength(255);
+        builder.Property(e => e.UpdatedAt)
+            .HasPrecision(0)
+            .IsRowVersion()
+            .HasDefaultValueSql(DbConstants.SysUtcDateTime);
+    }
+}
